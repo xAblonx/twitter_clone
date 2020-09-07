6076ff83649c31fbd257a153eb7339ef92b68c5e
Rennan Marcos <rennan@inttegra.com>
2020-09-07 09:34:46 -0300

    my first commit

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+  namespace App\Controllers;
+
+  use MF\Controller\Action;
+
+  class IndexController extends Action {
+
+    public function index() {
+
+      $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+      $this->render('index');
+    }
+
+    public function sobreNos() {
+
+      $this->view->dados = array('Notebook', 'Smartphone');
+      $this->render('sobreNos');
+    }
+  }
+?>
