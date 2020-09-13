bd01833b6b6ab70638805777dcd5e037be93cb31
Rennan Marcos Balan <rennanbalan@gmail.com>
2020-09-13 09:38:57 -0300

    Projeto finalizado

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -5,14 +5,22 @@
   use MF\Model\Container;
 
   class AppController extends Action { 
+
     public function timeline() {
 
       $this->validaAutenticacao();
 
       $tweet = Container::getModel('Tweet');
       $tweet->__set('id_usuario', $_SESSION['id']);
-      $tweets = $tweet->getAll();
 
+      $total_registros_pagina = 10;
+      $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+      $deslocamento = ($pagina - 1) * $total_registros_pagina;
+      $tweets = $tweet->getPorPagina($total_registros_pagina, $deslocamento);
+      $total_tweets = $tweet->getTotalRegistros();
+      
+      $this->view->total_de_paginas = ceil($total_tweets['total'] / $total_registros_pagina);
+      $this->view->pagina_ativa = $pagina;
       $this->view->tweets = $tweets;
 
       $usuario = Container::getModel('Usuario');
--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -48,6 +48,45 @@
       return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 
+    public function getPorPagina($limit, $offset) {
+      $query = "SELECT t.id, 
+                       u.nome, 
+                       t.id_usuario, 
+                       t.tweet, 
+                       DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') AS data
+                  FROM tweets AS t 
+             LEFT JOIN usuarios AS u 
+                    ON t.id_usuario = u.id 
+                 WHERE id_usuario = :id_usuario 
+                    OR t.id_usuario IN(SELECT id_usuario_seguindo 
+                                         FROM usuarios_seguidores 
+                                        WHERE id_usuario = :id_usuario)
+              ORDER BY t.data DESC 
+                 LIMIT $limit 
+                OFFSET $offset";
+      $stmt = $this->db->prepare($query);
+      $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+      $stmt->execute();
+
+      return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getTotalRegistros() {
+      $query = "SELECT COUNT(*) AS total
+                  FROM tweets AS t 
+             LEFT JOIN usuarios AS u 
+                    ON t.id_usuario = u.id 
+                 WHERE id_usuario = :id_usuario 
+                    OR t.id_usuario IN(SELECT id_usuario_seguindo 
+                                         FROM usuarios_seguidores 
+                                        WHERE id_usuario = :id_usuario)";
+      $stmt = $this->db->prepare($query);
+      $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+      $stmt->execute();
+
+      return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
     public function deleteTweet() {
       $query = "DELETE FROM tweets WHERE id = :id AND id_usuario = :id_usuario";
       $stmt = $this->db->prepare($query);
